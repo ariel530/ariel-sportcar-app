5593c3b5d64db498c32151abb03678d98b5c54bc
Ariel Nimni <arielvod67@gmail.com>
2020-06-30 18:23:11 +0200

    first commit

--- src/app/sportcars/sportcars.component.ts
+++ src/app/sportcars/sportcars.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Sportcar } from '../sportcar';
 import { SPORTCARS_LIST } from '../list-sportcars';
+import { RACES_LIST } from '../list-races';
 
 @Component({
   selector: 'app-sportcars',
@@ -11,6 +12,7 @@ export class SportcarsComponent implements OnInit {
   
   sportcars = SPORTCARS_LIST;
   selectedSportcar: Sportcar;
+  races = RACES_LIST;
 
   constructor() { }
 
